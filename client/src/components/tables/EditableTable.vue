531120b25625601c324a7eede88956f7a67953fe
Alexis Maldonado Aleman <alexis@blacklabs.mx~>
2019-03-15 13:45:42 -0700

    Se agrega empty state

--- client/src/components/tables/EditableTable.vue
+++ client/src/components/tables/EditableTable.vue
@@ -2,6 +2,7 @@
     <div>
         <div class="row m-0 w-100">
             <div class="col-12">
+                <div v-if="docs && docs.length">
                 <div class="card w-100">
                     <div class="card-header">
                         <TableHeaderSearch/>
@@ -12,33 +13,69 @@
                     </div>
                     <div class="card-body">
                         <div class="table-responsive">
-                            <table class="table table-hover form-table">
-                                <thead>
-                                <tr>
-                                    <TableTh :name="$t('suppliers.name')"/>
-                                    <TableTh :name="$t('suppliers.rfc')"/>
-                                    <TableTh :name="$t('suppliers.notes')"/>
-                                    <TableTh :name="$t('general.created-at')"/>
-                                    <!--<th>Title Header<i class="zmdi zmdi-caret-down m-l-5 f-16"></i><i></i></th>-->
-                                    <th></th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr v-for="(doc, index) in docs" :key="`doc-${index}`">
-                                    <td>{{doc.nombre}}</td>
-                                    <td>{{doc.rfc}}</td>
-                                    <td>{{doc.notas}}</td>
-                                    <td></td>
-                                    <!--<td></td>-->
-                                    <TableTdButtons/>
-                                </tr>
-                                </tbody>
-                            </table>
+                                <table class="table table-hover form-table">
+                                    <thead>
+                                    <tr>
+                                        <TableTh :name="$t('suppliers.name')"/>
+                                        <TableTh :name="$t('suppliers.rfc')"/>
+                                        <TableTh :name="$t('suppliers.notes')"/>
+                                        <TableTh :name="$t('general.created-at')"/>
+                                        <!--<th>Title Header<i class="zmdi zmdi-caret-down m-l-5 f-16"></i><i></i></th>-->
+                                        <th></th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+
+                                    <tr v-for="(doc, index) in docs" :key="`doc-${index}`">
+                                        <td>{{doc.nombre}}</td>
+                                        <td>{{doc.rfc}}</td>
+                                        <td>{{doc.notas}}</td>
+                                        <td></td>
+                                        <!--<td></td>-->
+                                        <TableTdButtons/>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <Pagination 
-                    :store-module="storeModule"
+                <div v-else>
+                    <div class="row m-b-50">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="empty-state">
+                                    <img class="img-fluid"
+                                         src="@/assets/images/Emptystates/empty-state-box.svg"
+                                         alt="Empty"/>
+                                    <p>
+                                        <strong class="d-block">Agrega usuarios a la
+                                            Organización.</strong>
+                                        Los usuarios invitados a la Organización recibirán un correo
+                                        electrónico con un enlace para acceder a la plataforma.
+                                    </p>
+                                    <a href="" class="btn-raised button-accent"><i
+                                            class="zmdi zmdi-plus"></i> NUEVO PROVEEDOR</a>
+                                </div>
+                                <div v-if="false"> <!-- If there's a query-->
+                                    <div class="empty-state">
+                                        <img class="img-fluid"
+                                        src="@/assets/images/Emptystates/empty-state-box.svg"
+                                        alt="Empty"/>
+                                        <p>
+                                            No se encontraron resultados con la búsqueda <strong>“Alicia
+                                            Martinez”.</strong>
+                                        </p>
+                                    </div>
+                                    <div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <Pagination
+                        :store-module="storeModule"
                 />
             </div>
         </div>
