b97d539fcc5088963409adf5167318f02d23734c
Raúl Maese <rmaese@blacklabs.mx>
2019-03-20 18:28:00 +0000

    Feat/editable table component

--- client/src/pages/admin/suppliers/Suppliers.vue
+++ client/src/pages/admin/suppliers/Suppliers.vue
@@ -2,8 +2,10 @@
     <div>
         <BackButton/>
         <CatalogHeader/>
-        <EditableTable 
-            :docs="docs" 
+        <EditableTable
+            :docs="docs"
+            :tableHeaders="tableHeaders"
+            :tableColumns="tableColumns"
             :store-module="storeModule"
         />
     </div>
@@ -14,20 +16,23 @@
 
 <script>
     import catalog from '@/mixins/catalog.mixin';
-    
     const storeModule = 'suppliers';
     const docName = 'suppliers.supplier';
-    
+
     let catalogoProveedores = catalog.configure({
         storeModule: storeModule,
         docName: docName
     });
-    
+
     export default {
         mixins: [catalogoProveedores],
         data () {
             return {
-                storeModule: storeModule
+                storeModule: storeModule,
+                tableHeaders : ['suppliers.name','suppliers.rfc','suppliers.notes','general.created-at'],
+                tableColumns: [
+                    {field:'name'}, {field:'rfc'}, {field:'notes'},{field:'createdAt'}
+                ]
             }
         },
         components: {
