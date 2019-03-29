1bb3ffd4a704b31b9a65552546ae2f390b92efd8
Jorge Hermosillo Salcido <jhermosillo@blacklabs.mx>
2019-03-29 12:42:53 -0700

    Cliente Heroku

--- client/server.js
+++ client/server.js
@@ -3,6 +3,6 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
-var port = 80;
+var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
