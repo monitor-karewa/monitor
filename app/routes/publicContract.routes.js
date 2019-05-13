a2b426f2c2d4ebe8762141e4999213018c546c80
Alexis Maldonado Aleman <alexis@blacklabs.mx~>
2019-05-13 17:19:26 -0600

    Se implementa filtro de contratos

--- app/routes/publicContract.routes.js
+++ app/routes/publicContract.routes.js
@@ -10,6 +10,12 @@ var securityController = require('./../controllers/security.controller');
  */
 router.get('/list', securityController.validatePermission(Contract.permission, 'read'), publicContractController.list);
 
+/**
+ * GET /list
+ * Consulta de registros
+ */
+router.post('/filtered-list', securityController.validatePermission(Contract.permission, 'read'), publicContractController.list);
+
 
 /**
  * GET /list
@@ -29,6 +35,44 @@ router.get('/totals', securityController.validatePermission(Contract.permission,
  * Load Contract detail
  */
 router.get('/detail', publicContractController.detail);
+
+
+/**
+ * GET /retrieve/suppliers
+ * Obtains the suppliers available according to the currenct contracts
+ */
+router.get('/retrieve/suppliers', securityController.validatePermission(Contract.permission, 'read'), publicContractController.retrieveSuppliers);
+
+/**
+ * GET /retrieve/administrative-units
+ * Obtains the administrative units available according to the currenct contracts
+ */
+router.get('/retrieve/administrative-units', securityController.validatePermission(Contract.permission, 'read'), publicContractController.retrieveAdministrativeUnits);
+
+/**
+ * GET /retrieve/trimonths
+ * Obtains the administrative units available according to the currenct contracts
+ */
+router.get('/retrieve/trimonths', securityController.validatePermission(Contract.permission, 'read'), publicContractController.retrieveTrimonths);
+
+/**
+ * GET /retrieve/retrieveAdministration-periods
+ * Obtains the administration periods available according to the currenct contracts
+ */
+router.get('/retrieve/administration-periods', securityController.validatePermission(Contract.permission, 'read'), publicContractController.retrieveAdministrationPeriods);
+
+
+/**
+ * GET /retrieve/fiscal-years
+ * Obtains the fiscal years available according to the currenct contracts
+ */
+router.get('/retrieve/fiscal-years', securityController.validatePermission(Contract.permission, 'read'), publicContractController.retrieveFiscalYears);
+
+/**
+ * GET /retrieve/fiscal-years
+ * Obtains the fiscal years available according to the currenct contracts
+ */
+router.get('/retrieve/procedure-types', securityController.validatePermission(Contract.permission, 'read'), publicContractController.retrieveProceudureTypes);
 
 
 
--- client/src/store/modules/publicContracts.store.js
+++ client/src/store/modules/publicContracts.store.js
@@ -24,7 +24,13 @@ const state = {
         PUBLIC: 0.00,
         INVITATION: 0.00
     },
-    contractDetail : {}
+    contractDetail : {},
+    adminstrativeUnitsForFilter : [],
+    fiscalYears : [],
+    trimonths : [],
+    administrationPeriods : [],
+    procedureTypes : [],
+    lastQuery : {}
 };
 
 const getters = {
@@ -51,7 +57,7 @@ const getters = {
 
 const actions = {
     /**
-     * Retrieves suppliers from DB to make them available at selection
+     * Retrieves administrative units from DB to make them available at selection
      */
     getAdministrativeUnits({commit}){
         contractsApi.retrieveAdministrativeUnits({},
@@ -61,13 +67,6 @@ const actions = {
     },
 
     list ({commit,getters}, searchString ) {
-        console.log("publicContracts.store#list");
-        if(searchString && searchString.length){
-            commit('SET_SEARCH',searchString);
-            commit('UPDATE_PAGE',1);
-        } else {
-            commit('SET_SEARCH',"");
-        }
         let query = getters.getUrlQuery;
 
         contractsApi.list(
@@ -98,28 +97,12 @@ const actions = {
         }
         return "";
     },
-    changePage ({commit, getters, state}, page) {
-        console.log('page --> ' + page);
+    changePage ({commit, getters, state, dispatch}, page) {
         let oldPage = state.pagination.page;
         commit('UPDATE_PAGE',page);
-        let query = getters.getUrlQuery;
-        contractsApi.list(
-            { query },
-            (result) => {
-                //result.data.data.docs
-                // commit('updateDocs', {
-                //     docs: result.data.data.docs
-                // });
-                commit('SET_CONTRACTS', result.data.data);
-            },
-            (error) => {
-                Vue.$log.error('Response error', error);
-                tShow(`Hubo un error en el paginado: ${error}`);
-            }
-        )
+        dispatch(`${storeName}/loadFilteredList`,state.lastQuery,{root:true});
     },
     loadContractDetail({commit}, id) {
-
         let query = `?id=${id}`;
         contractsApi.detail({query}, (result) => {
             commit('SET_CONTRACT_DETAIL', result.data);
@@ -127,6 +110,64 @@ const actions = {
             tShow(i18n.t('suppliers.public.load.error'), 'danger');
             commit('SET_CONTRACT_DETAIL', {});
         })
+    },
+    getAdministrativeUnitsForFilter({commit}){
+        contractsApi.retrieveAdministrativeUnitsForFilter({},
+            (result)=>{
+                commit('SET_ADMINISTRATIVE_UNITS_FILTER', result.data);
+            })
+    },
+    getFiscalYears({commit}){
+        contractsApi.retrieveFiscalYears({},
+            (result)=>{
+                commit('SET_FISCAL_YEARS', result.data);
+            })
+    },
+    getTrimonths({commit}){
+        contractsApi.retrieveTrimonths({},
+            (result)=>{
+                commit('SET_TRIMONTHS', result.data);
+            })
+    },
+    getAdministrationPeriods({commit}){
+        contractsApi.retrieveAdministrationPeriods({},
+            (result)=>{
+                commit('SET_ADMINISTRATION_PERIODS', result.data);
+            })
+    },
+    getProcedureTypes({commit}){
+        contractsApi.retrieveProceudureTypes({},
+            (result)=>{
+                if(result && result.data && result.data.length){
+                    let procedureTypes = result.data.map(function (item) {
+                        return item._id;
+                    });
+                    commit('SET_PROCEDURE_TYPES', procedureTypes);
+                }
+            })
+    },
+    filter({commit, dispatch},filters){
+        commit('UPDATE_PAGE',1);
+        dispatch(`${storeName}/loadFilteredList`,filters,{root:true});
+    },
+    loadFilteredList({commit, getters},filters){
+        let query = getters.getUrlQuery;
+
+        commit('SET_LAST_QUERY',filters);
+
+        contractsApi.filteredList(
+            {
+                query: query,
+                filters : filters
+            },
+            (result) => {
+                commit('SET_CONTRACTS', result.data.data);
+            },
+            (error) => {
+                Vue.$log.error('Response error', error);
+                tShow(`Hubo un error al cargar el listado : ${error}`);
+            }
+        )
     }
     };
 
@@ -168,7 +209,33 @@ const mutations = {
     },
     SET_CONTRACT_DETAIL (state, detail) {
         state.contractDetail = detail.data;
-    }
+    },
+    SET_ADMINISTRATIVE_UNITS_FILTER (state, administrativeUnits) {
+        state.adminstrativeUnitsForFilter = administrativeUnits;
+    },
+    SET_FISCAL_YEARS (state, fiscalYears) {
+        state.fiscalYears = fiscalYears;
+    },
+    SET_TRIMONTHS (state, trimonths) {
+        state.trimonths = trimonths;
+    },
+    SET_ADMINISTRATION_PERIODS (state, administrationPeriods) {
+        state.administrationPeriods = administrationPeriods;
+    },
+    SET_PROCEDURE_TYPES(state, procedureTypes) {
+        state.procedureTypes = procedureTypes;
+    },
+    /**
+     * Saves which was the last query used so it can change page when there are filtered results
+     */
+    SET_LAST_QUERY(state, lastQuery) {
+        state.lastQuery.administrationPeriods = lastQuery.administrationPeriods;
+        state.lastQuery.fiscalYears = lastQuery.fiscalYears;
+        state.lastQuery.trimonths = lastQuery.trimonths;
+        state.lastQuery.procedureTypes = lastQuery.procedureTypes;
+        state.lastQuery.administrativeUnits = lastQuery.administrativeUnits;
+        state.lastQuery.search = lastQuery.search;
+    },
 };
 
 
