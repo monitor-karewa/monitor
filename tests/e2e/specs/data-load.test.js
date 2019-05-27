288d75e7bcad646b96c7f784464c857eaed49fcf
Karen Villa <kvilla@blacklabs.mx>
2019-05-27 10:31:32 -0600

    Se agregan las pruebas automatizadas en la carga de datos, incluyendo un archivo de prueba

--- tests/e2e/specs/data-load.test.js
+++ tests/e2e/specs/data-load.test.js
@@ -42,9 +42,27 @@ module.exports = {
 
             })
 
+            .waitForElementVisible(utils.xpath.elementWithText("button", "Cargar archivo"), 1000)
+            .setValue(utils.xpath.elementWithId("input","file"),require('path').resolve(__dirname + '/dataLoadTest.xlsx'))
+
+            .pause(1000)
+            .waitForElementVisible(utils.xpath.elementWithText("button", "Mostrar detalles"), 1000)
+            .click('//button[contains(text(),"Mostrar detalles")]')
+
+            .pause(1000)
+            .waitForElementVisible(utils.xpath.elementWithText("button", "Ocultar detalles"), 1000)
+            .click('//button[contains(text(),"Ocultar detalles")]')
+
+            .pause(1000)
+            .waitForElementVisible(utils.xpath.elementWithText("button", "Cancelar procesamiento"), 1000)
+            .execute(function(){
+
+                let buttonsMenu = document.querySelector('button.btn-outline.c-error').click()
+
+            })
 
 
-            .pause(5000)
+            .pause(1000)
             .end();
     }
 
