e88484d564b6e94653fc249ebbfc438dd5cec25e
Normando Ali Zubia Hernandez <nzubia@blacklabs.mx>
2019-09-12 11:06:32 -0600

    Se modifican valores para dejar archivo génerico

--- src/karewaPlatform.info.js
+++ src/karewaPlatform.info.js
@@ -1,44 +1,34 @@
 export default  {
     // Información dinámica que aparecerá en las vista "Acerca de" de la plataforma
     infoAcercaDe: {
-        textoAcercaDe: "<strong class=\"f-24 c-accent principal-font-medium\">Karewa</strong> es una agrupación ciudadana sin fines de lucro, que desde agosto de 2016 trabaja con el objetivo de <strong>prevenir la corrupción</strong> en los procesos de compra y contratación de los gobiernos de la ciudad de Chihuahua. Todo esto a través de un programa estratégico de transparencia y rendición de cuentas a fin de contar con un eficiente y mejor gobierno.",
+        textoAcercaDe: "<strong class=\"f-24 c-accent principal-font-medium\">Nombre Organizacion</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         imagenAcercaDe: "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-        mision: "Eliminar la corrupción en el manejo de los recusros públicos.",
-        vision: "Somos la organización más influyente en México en el combate a la corrupción del manejo de los recursos públicos.",
-        valores: "Nos regimos por la honestidad. el respeto. el compromiso. el trabajo en equipo y sobre todo con justicia.",
+        mision: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        vision: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        valores: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         consejoCoordinacion: [
-            { nombre: "José David Ponce de León Trejo", puesto: "Coordinación"},
-            { nombre: "Edgar Renteria Rivas",  puesto: "Coordinación"}
+            { nombre: "Nombre Persona", puesto: "Puesto Persona"}
         ],
         consejo: [
-            { nombre: "Javier Mesta Delgado", puesto: "Consejero Presidente"},
-            { nombre: "Gabriela Alvídrez Ruiz",  puesto: "Consejera Directiva"},
-            { nombre: "Elías Fernando Ayub González",  puesto: "Consejero Directivo"},
-            { nombre: "Paola Contestábile Frayre",  puesto: "Consejera Directiva"},
-            { nombre: "César René Espinosa Lozano",  puesto: "Consejero Directivo"},
-            { nombre: "Luis Alberto González Herrera",  puesto: "Consejero Directivo"},
-            { nombre: "Leopoldo Rafael Lozano Cuarón",  puesto: "Consejero Directivo"},
-            { nombre: "Guillermo Antonio Orozco Salomón",  puesto: "Consejero Directivo"},
-            { nombre: "Luis Alonso Ramos Fernández",  puesto: "Consejero Directivo"},
-            { nombre: "Angélica Santana Fierro",  puesto: "Consejera Directiva"},
+            { nombre: "Nombre Persona", puesto: "Puesto Persona"}
         ],
         imagenConsejo: "https://images.unsplash.com/photo-1522165078649-823cf4dbaf46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80"
 
     },
     // Información dinámica que aparecerá en las vista "Login" de la plataforma
     infoLogin: {
-        nombreOrganizacion: "KAREWA",
-        lemaOrganizacion: "Es Amar Chihuahua",
+        nombreOrganizacion: "Nombre Organización",
+        lemaOrganizacion: "Lorem ipsum dolor sit amet",
         descripcionEnParrafos: [
-            "Karewa es una agrupación ciudadana sin fines de lucro, que desde agosto de 2016 trabaja con el objetivo de prevenir la corrupción en los procesos de compra y contratación de los gobiernos de la ciudad de Chihuahua.",
-            "Somos la organización más influyente en México en el combate a la corrupción del manejo de los recursos públicos."
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         ],
-        enlaceWebsite: "https://karewa.org",
-        nombreWebsite: "karewa.org"
+        enlaceWebsite: "https://direccionSition.dominio",
+        nombreWebsite: "direccionSition.dominio"
 
     },
     // Información dinámica que define la fuente de información de los datos presentados en las vistas de: Contratos y Proveedores
     infoFuenteInformacion: {
-        fuenteInformacion: "http://www.municipiochihuahua.gob.mx/Transparencia"
+        fuenteInformacion: "http://www.plataformadetransparencia.org.mx/"
     }
 }
